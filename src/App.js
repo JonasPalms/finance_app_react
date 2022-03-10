1fc793ce8b84a522f44631dee5ba0b551e28722b
Jonas Palm <jonas.p.sorensen@gmail.com>
2022-03-10 16:01:33 +0100

    lesson 115

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css'
+
+
+// components
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
 
 function App() {
   return (
-    <div className="App">
 
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,50 @@
+// hooks 
+import { useState } from 'react'
+// styles
+import styles from './Signup.module.css'
+
+import React from 'react'
+
+
+export default function Signup() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(email, password, username)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className={styles['signup-form']}>
+            <h2>Signup</h2>
+            <label >
+                <span>email:</span>
+                <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>password:</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password} />
+            </label>
+            <label>
+                <span>username</span>
+                <input
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username} />
+            </label>
+
+            <button className='btn'>Submit</button>
+        </form>
+    )
+}
